08369b0cedbdefcee1735c9c4c3dc6813854cf9b
kf32 <a2032kf@aiit.ac.jp>
2020-07-04 22:33:19 +0900

    README及びテストスクリプト更新

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-初回Gitに向けて
-# kadai4
+■課題4の説明
+
+1. kadai4.shについて
+最大公約数を算出するスクリプトです。
+以下のように、引数を2つ与えて実行することで機能します。
+
+bash /home/kf32/kadai4/kadai4.sh 100 8
+→4が回答される
+
+
+2. checker.shについて
+kadai4.shをテストするスクリプトです。
+引数が無かった場合、引数が1つだけしか与えられなかった場合、
+0と0を与えた場合でテストを行います。
+
+3. 補足
+GitHubのレポジトリ
+https://github.com/Keiji0213/kadai4
+
+Travis CIのURL
+https://travis-ci.com/github/Keiji0213/kadai4
+
+
+以上
--- checker.sh
+++ checker.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-#実行結果(引数入力漏れ)
-./kadai4.sh > /tmp/result-$$
+#テスト(引数入力漏れ)
+./kadai4.sh 2> /tmp/result-$$ && exit 1
 #エラー出力
-echo "引数がありません" > /tmp/ans-$$
+echo "" > /tmp/ans-$$
 
 diff /tmp/ans-$$ /tmp/result-$$ || exit 1
 
-#実行結果（引数が1つしか無い場合）
-./kadai4.sh $1 > /tmp/result-$$
+#テスト（引数が1つしか無い場合）
+./kadai4.sh $1 2> /tmp/result-$$ && exit 1
 #エラー出力
-echo "引数が1つしかありません" > /tmp/ans-$$
+echo "" 2> /tmp/ans-$$ &&
+
+diff /tmp/ans-$$ /tmp/result-$$ || exit 1
+
+#テスト（引数に0を与えた場合）
+.kadai4.sh 0 0 2> /tmp/result-$$ && exit 1
+echo "" > /tmp/ans-$$
 
 diff /tmp/ans-$$ /tmp/result-$$ || exit 1
 
