ead182b534f176550a62676cc53d9355bad8a991
kf32 <a2032kf@aiit.ac.jp>
2020-07-05 17:33:18 +0900

    最終チェック予定

--- checker.sh
+++ checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-assert_equal() {
+kadai4_check() {
 	if [ "$1" != "$2" ]; then
 		echo "$3" >&2
 		exit 1
@@ -10,13 +10,15 @@ assert_equal() {
 
 #テスト(引数の個数確認)
 message="2つの引数を指定してください"
-assert_equal "${message}" "$(./kadai4.sh 2>&1)" "引数無し"
-assert_equal "${message}" "$(./kadai4.sh 2 2>&1)" "引数不足"
-assert_equal "${message}" "$(./kadai4.sh 3 6 9 2>&1)" "引数過多"
+kadai4_check "${message}" "$(./kadai4.sh 2>&1)" "引数無し"
+kadai4_check "${message}" "$(./kadai4.sh 2 2>&1)" "引数不足"
+kadai4_check "${message}" "$(./kadai4.sh 3 6 9 2>&1)" "引数過多"
 
 #テスト（動作確認 引数1 > 引数2）
-assert_equal "5" "$(./kadai4.sh 100 5 2>&1)" "計算エラー 引数1 > 引数2"
+kadai4_check "5" "$(./kadai4.sh 100 5 2>&1)" "計算エラー 引数1 > 引数2"
 
 #テスト（動作確認 引数2 > 引数1）
-assert_equal "2" "$(./kadai4.sh 2 200 2>&1)" "計算エラー 引数2 > 引数1"
+kadai4_check "2" "$(./kadai4.sh 2 200 2>&1)" "計算エラー 引数2 > 引数1"
+
+kadai4_check "" "$(.kadai4.sh 0 0 2>&1)" "ゼロ同士"
 
