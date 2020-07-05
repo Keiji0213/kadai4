94930e1e3346797bb2dd051f3a49e80ff2bca4e1
kf32 <a2032kf@aiit.ac.jp>
2020-07-05 17:18:38 +0900

    テスト再修正

--- checker.sh
+++ checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 assert_equal() {
-	if [ "$1" != "$2 ]; then
+	if [ "$1" != "$2" ]; then
 		echo "$3" >&2
 		exit 1
 	fi
@@ -17,4 +17,6 @@ assert_equal "${message}" "$(./kadai4.sh 3 6 9 2>&1)" "引数過多"
 #テスト（動作確認 引数1 > 引数2）
 assert_equal "5" "$(./kadai4.sh 100 5 2>&1)" "計算エラー 引数1 > 引数2"
 
+#テスト（動作確認 引数2 > 引数1）
+assert_equal "2" "$(./kadai4.sh 2 200 2>&1)" "計算エラー 引数2 > 引数1"
 
