7e784e33d43df9425d03630d0e3a7c8d8301d5d8
kf32 <a2032kf@aiit.ac.jp>
2020-07-05 16:57:39 +0900

    テスト再修正

--- checker.sh
+++ checker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#テスト(正常動作確認)
-./kadai4.sh 2 4 && echo 2 && exit 1 || 2> /tmp/result-$$
+#テスト(引数の個数確認)
+message="2つの引数を指定してください"
+assert_equal "${message}" "$(./kadai4.sh 2>&1)" "引数無し"
+assert_equal "${message}" "$(./kadai4.sh 2 2>&1)" "引数不足"
+assert_equal "${message}" "$(./kadai4.sh 3 6 9 2>&1)" "引数過多"
 
-#テスト（引数が1つしか無い場合）
-./kadai4.sh $1 2> /tmp/result-$$ && exit 1
+#テスト（動作確認 $1 > $2）
+assert_equal "5" "$(./kadai4.sh 100 5 2>&1)" "計算エラー $1 > $2"
 
-#テスト（引数に0を与えた場合）
-.kadai4.sh 0 0 && echo "" && exit 1 || 2> /tmp/result-$$
+#テスト（動作確認 $2 > $1)
+assert_equal "2" "$(./kadai4.sh 2 100 2>&1)" "計算エラー $2 > $$1"
 
-echo "complete"
--- kadai4.sh
+++ kadai4.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+	echo "2つの引数を指定してください" 1>&2
+	exit 1
+fi
+
 echo $1 $2 |
  awk '{while($1*$2!=0)
   {if($1>$2){$1=$1-$2}
