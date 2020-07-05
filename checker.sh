f1126952ce1adb077e4ebb147c90eb9b75d702e8
kf32 <a2032kf@aiit.ac.jp>
2020-07-05 17:12:53 +0900

    テスト再修正

--- checker.sh
+++ checker.sh
@@ -14,9 +14,9 @@ assert_equal "${message}" "$(./kadai4.sh 2>&1)" "引数無し"
 assert_equal "${message}" "$(./kadai4.sh 2 2>&1)" "引数不足"
 assert_equal "${message}" "$(./kadai4.sh 3 6 9 2>&1)" "引数過多"
 
-#テスト（動作確認 $1 > $2）
-assert_equal "5" "$(./kadai4.sh 100 5 2>&1)" "計算エラー $1 > $2"
+#テスト（動作確認 引数1 > 引数2）
+assert_equal "5" "$(./kadai4.sh 100 5 2>&1)" "計算エラー 引数1 > 引数2"
 
-#テスト（動作確認 $2 > $1)
-assert_equal "2" "$(./kadai4.sh 2 100 2>&1)" "計算エラー $2 > $1"
+#テスト（動作確認 引数2 > 引数1)
+assert_equal "2" "$(./kadai4.sh 2 100 2>&1)" "計算エラー 引数2 > 引数1"
 
